f9a09940491cb30185d0c3de18a876dfeb32c692
vilenani <vilena.niyaz@gmail.com>
2021-12-13 15:52:35 +0500

    lesson22

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,35 +2,40 @@ import React from 'react';
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-const DialogItem = () => {
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
     return (
         <div className={s.dialog + ' ' + s.active}>
-            <NavLink to ='/dialog/2'>Maxim</NavLink>
+            <NavLink to ={path}>{props.name}</NavLink>
         </div>
     )
+}
+
+const Message = (props) => {
+    return (
+        <div className={s.messages}>
+            <div className={s.message}>Hello</div>
+            <div className={s.message}>How are you</div>
+            <div className={s.message}>Thanks</div>
+            <div className={s.messages}>
+                <NavLink to></NavLink>
+            </div>
+        </div>
+        )
+
 }
 
 const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem />
+                <DialogItem name='Maxim' id='1'/>
+                <DialogItem name='Kate' id='2'/>
+                <DialogItem name='Denis' id='3'/>
+                <DialogItem name='Danya' id='1'/>
+                <DialogItem name='Ann' id='1'/>
+            </div>
 
-                <div className={s.dialog}>
-                    <NavLink to='/dialog/3'>Kate</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialog/4'>Danya</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialog/5'>Denis</NavLink>
-                </div>
-            </div>
-            <div className={s.messages}>
-                <div className={s.message}>Hello</div>
-                <div className={s.message}>How are you</div>
-                <div className={s.message}>Thanks</div>
-            </div>
         </div>
     )
 }
